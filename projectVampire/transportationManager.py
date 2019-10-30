7a0d4e86b6e00801efb3b509e71f4538a3b8c1ca
d-Nic <z5205187@student.unsw.edu.au>
2019-10-30 13:47:39 +1100

    adjusted to match uml

--- projectVampire/transportationManager.py
+++ projectVampire/transportationManager.py
@@ -1,38 +1,20 @@
 class transportationManager():
- 
-    def __init__(self, location):
-        self.location = location
- 
-    # Given blood, send it to its destination 
-    def sendBlood(self, blood):
-        # Get blood destination from system
- 
-        # Get routing path for blood from routing
- 
-        # Give blood to transporter (don't need this)
- 
-        print('Ready to send blood')
-    
-    def bloodArrival(self, blood):
-        # If blood did not arrive at destination
-            # If blood was going to a recipient, need to 
-            # make a request for blood of same type to recipient's
-            # destination
-        # Else 
-            # Confirm arrival
-            # Dispatch to destination
- 
-        # If blood is lost from donation to pathology or
-            # pathology to storage no need to do anything
-        print('Success of blood arrival has been')
- 
-    def dispatchBlood(self, blood):
-        # Blood has successfully arrived at the destination, dispatch it
-        # If blood needs to be verified
-            # Dispatch to pathologist
-        # If blood is to be stored
-            # Dispatch to storage manager
-        # If blood is to be delivered to recipient 
-            # Dipatch blood to recipient     
+
+    def __init__(self, destinations, location):
+        self.locale = location # location of this transport manager
+        self.destinations = destinations
+        self.toSend = [] # blood that is to be sent
+
+    # Get blood
+    def receive(self, blood):
+        self.toSend.append(blood)
+
+    def addRoute(self, location, route):
+        # add route to destinations
+        destinations[location] = route;
+
+    def dispatchBlood(self, blood, location):
+        #location.recieveBlood(blood)
+        self.toSend.remove(blood)
         print('Blood has been dispatched')
 
