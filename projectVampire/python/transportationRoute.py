88cd8cfae083a4b1d90423fd96217ef5afae46ae
d-Nic <z5205187@student.unsw.edu.au>
2019-11-16 21:52:00 +1100

    transport nodes implemented

--- projectVampire/python/transportationRoute.py
+++ projectVampire/python/transportationRoute.py
@@ -1,154 +1,173 @@
 import math
 import sys
+import time
 MAX_DIST=20
 
+
+# locations to help simulate the path the blood will take
 class transportNode:
-    
-    def __init__(self, x, y, name):
-       self._x = x
-       self._y = y
-       self.name = name
-       self._connected = []
+	def __init__(self, name, x, y, locale):
+	   self._x = x
+	   self._y = y
+	   self.name = name
+	   self._connected = []
+	   self._locale = locale
 
-    def getX(self):
-        return self._x
+	def getX(self):
+		return self._x
 
-    def getY(self):
-        return self._y
+	def getY(self):
+		return self._y
 
-    def getConnected(self):
-        return self._connected
+	def getConnected(self):
+		return self._connected
 
-    def addConnection(self, node):
-        self._connected.append(node)
+	def addConnection(self, node):
+		self._connected.append(node)
 
 class transportationRoute:
 
-    def __init__(self, src, dest, world):
-        self._world = world
-        self._src = src
-        self._dest = dest
+	def __init__(self, world):
+		self._world = world            
 
-    def getX(self):
-       return self._src.getX()
+	def getConnected(self):
+	   return self._src.getConnected()
 
-    def getY(self):
-       return self._src.getY()               
+	def displayPath(self, path):
+		for node in path:
+			print("(",node.getX(), node.getY(),") -> ",end='')
+		print("X")
 
-    def getConnected(self):
-       return self._src.getConnected()
-
-    def atDest(self, node):
-        if node.getX() == self._dest.getX() and node.getY() == self._dest.getY():
-            return True
-        return False 
-
-    def displayPath(self, path):
-        for node in path:
-            print("(",node.getX(), node.getY(),") -> ",end='')
-        print("X")
-
-    # gives a list of nodes to arrive at dest (requires a connected graph to work)
-    def generatePath2(self, nodes):
-        curr = self
-        path = [src]
-        visited = [src]
-        while not self.atDest(curr):
-            currDist = MAX_DIST
-            chosen = None
-            for node in curr.getConnected():
-                if node not in path and node != curr:
-                    print(curr.getX(),curr.getY(), "->",node.getX(),node.getY())
-                    if node == dest:
-                        chosen = dest
-                        break
-                    # Use node with least weight
-                    cDist = math.sqrt((curr.getX() - node.getX())**2 + ((curr.getY() - node.getY()))**2)
-                    if (cDist < currDist):
-                        chosen = node
-                        currDist = cDist
-            path.append(chosen)
-            curr = chosen
-        return path
+	# gives a list of nodes to arrive at dest (requires a connected graph to work)
+	def generatePath2(self, nodes):
+		curr = self
+		path = [src]
+		visited = [src]
+		while not self.atDest(curr):
+			currDist = MAX_DIST
+			chosen = None
+			for node in curr.getConnected():
+				if node not in path and node != curr:
+					print(curr.getX(),curr.getY(), "->",node.getX(),node.getY())
+					if node == dest:
+						chosen = dest
+						break
+					# Use node with least weight
+					cDist = math.sqrt((curr.getX() - node.getX())**2 + ((curr.getY() - node.getY()))**2)
+					if (cDist < currDist):
+						chosen = node
+						currDist = cDist
+			path.append(chosen)
+			curr = chosen
+		return path
 
 
-    # BFS traversal
-    def generatePath(self):
-        pred = [list() for i in range(len(self._world))]
-        dist = [-1] * len(self._world)
+	# BFS traversal
+	def generatePath(self, src):
+		pred = [list() for i in range(len(self._world))]
+		dist = [-1] * len(self._world)
 
-        dist[self._src.name] = 0
-        toCheck = [self._src]
+		dist[src.name] = 0
+		toCheck = [src]
 
-        while len(toCheck) > 0:
-            par = toCheck[0]
-            toCheck.remove(toCheck[0])
-            for node in par.getConnected():
-                if dist[node.name] < 0:
-                    pred[node.name].append(par)
-                    dist[node.name] = dist[par.name] + 1 #set this val
-                    toCheck.append(node)
-                elif dist[node.name] == dist[par.name] + 1:
-                    pred[node.name].append(par)
-        return pred, dist
+		while len(toCheck) > 0:
+			par = toCheck[0]
+			toCheck.remove(toCheck[0])
+			for node in par.getConnected():
+				if dist[node.name] < 0:
+					pred[node.name].append(par)
+					dist[node.name] = dist[par.name] + math.sqrt((par.getX() - node.getX())**2 + ((par.getY() - node.getY()))**2) #set this val
+					toCheck.append(node)
+				#elif dist[node.name] == dist[par.name] + 1:
+					#pred[node.name].append(par)
+		return pred, dist
 
-    # gives distance to destination
-    def getDist():
-        return 3
-    # gives the time taken to arrive at dest
-    def getTime():
-        return 3
+	def sendBlood(self, bloodTup, source):
+		blood = bloodTup[0]
+		dest = bloodTup[1]
+		path, dist = self.generatePath(source)
 
+		curr = dest
+		final = []
+		final.insert(0,curr)
 
-#testing
+		# reorder path
+		while curr.name != source.name:
+			curr = path[curr.name][0]
+			final.insert(0,curr)
 
-node1 = transportNode(1,5, 0)
+		prevTime = 0
+		for node in final[1::]:
+			print("Sending blood to", node.name, ", will take", round(dist[node.name]-prevTime,1),"seconds")
+			time.sleep(round(dist[node.name]-prevTime,1))
+			prevTime = dist[node.name]
+			#print(node.name, dist[node.name])
+		print("Blood has arrived at destination")
 
-node2 = transportNode(5,1, 1)
+		#dest.recieve(blood)
 
-node3 = transportNode(5,6, 2)
+# Testing
 
-node4 = transportNode(8,6, 3)
+# Creating nodes with transportNode(name of node, x value, y value)
+"""
+node0 = transportNode(0,1,5)
 
-node5 = transportNode(5,2, 4)
+node1 = transportNode(1,5,6)
 
-dest = transportNode(5,9, 5)
+node2 = transportNode(2,5,1)
 
-src = transportNode(8,1, 6)
+node3 = transportNode(3,8,1)
 
-node1.addConnection(node3)
-node1.addConnection(dest)
+node4 = transportNode(4,8,6)
 
-node2.addConnection(src)
+node5 = transportNode(5,5,9)
+
+node0.addConnection(node5)
+node0.addConnection(node1)
+
+node1.addConnection(node0)
+node1.addConnection(node4)
+node1.addConnection(node2)
+
+node2.addConnection(node1)
 node2.addConnection(node3)
-#node2.addConnection(node5)
 
-node3.addConnection(node1)
 node3.addConnection(node2)
 node3.addConnection(node4)
 
 node4.addConnection(node3)
-node4.addConnection(dest)
+node4.addConnection(node1)
+node4.addConnection(node5)
+
+node5.addConnection(node4)
+node5.addConnection(node0)
+
+world = [node0,node1,node2,node3,node4,node5]
 
 
-src.addConnection(node2)
-src.addConnection(node4)
+r = transportationRoute(world)
 
-dest.addConnection(node1)
-dest.addConnection(node4)
+# blood tup[Blood, dest]
+bld = [None, node1]
+# tup, src
+r.sendBlood(bld, node5)
 
-world = [src, dest, node1, node2, node3, node4, node5]
+#path, dist = r.generatePath(src)
+#counter = 0
 
+#curr = dest
+#print(curr.name, dist[curr.name])
+#final = []
+#final.insert(0,curr)
+#while curr.name != src.name:
+#	curr = path[curr.name][0]
+#	final.insert(0,curr)
+	#print(curr.name, dist[curr.name])
 
-r = route(src, dest, world)
-path, dist = r.generatePath()
-counter = 0
-
-curr = dest
-while curr.name != src.name:
-    curr = path[curr.name][0]
-    print(curr.name)
+#for node in final:
+#	print(node.name, dist[node.name])
 
 #print(path[5][0].name, path[3][0].name)
 #print(dist[3])
 #r.displayPath(path)
+"""
